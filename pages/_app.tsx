99cddd109cbc772fb28acf10710177ebcea8875a
Amey <amdhavle@me.com>
2025-04-13 13:38:57 -0400

    adding color theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-gradient-to-br from-[#f9f4ef] via-[#e8ddff] to-[#cfd8f9] text-neutral-900 min-h-screen">
-      <Component {...pageProps} />
+      <Navbar />
+      <main className="pt-20">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </div>
   );
 }
